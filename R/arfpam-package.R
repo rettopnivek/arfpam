13c036f2e50f970b9ac5df1c9b2792b8652d462d
rettopnivek <kevin.w.potter@gmail.com>
2021-03-24 20:42:22 -0400

    feat: Added license

---
+++ R/arfpam-package.R
@@ -0,0 +1,24 @@
+# Description for the 'arfpam' package
+# Written by Kevin Potter
+# email: kevin.w.potter@gmail.com
+# Please email me directly if you
+# have any questions or comments
+# Last updated 2021-03-24
+
+# Package development
+# library(devtools)
+# library(roxygen2)
+# devtools::test()
+
+#' Assorted R Functions for Processing, Analysis, and Modeling
+#'
+#' @docType package
+#' @name arfpam-package
+#'
+#' @description
+#'
+#' The \pkg{arfpam} package provides assorted
+#' functions to help with a variety of common
+#' tasks when processing, plotting, analyzing,
+#' and modeling data.
+NULL
